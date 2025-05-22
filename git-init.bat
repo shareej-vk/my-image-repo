d39f8cfc8044653555fce57c62b14dde31a7cdf3
Shareej <shareejvk@gmail.com>
2025-05-22 11:10:12 +0530

    Your commit message

---
+++ git-init.bat
@@ -0,0 +1,9 @@
+call git init
+call git add .
+call git commit -m "Your commit message"
+call git remote add origin https://github.com/shareej-vk/my-image-repo.git
+call git branch -M main
+call git push -u origin main
+
+
+
